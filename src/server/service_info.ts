e5fcead2c701f665b822655af114b51ae053431a
Max Nikitin <max@rururu.me>
2020-09-16 01:48:34 +0300

    fix service id

--- src/server/service_info.ts
+++ src/server/service_info.ts
@@ -4,6 +4,5 @@ import {
 
 // Generates a unique string identifier for service connection info.
 export function buildServiceInfoIdentifier(info: IGBServiceInfo): string {
-  // Return endpoint for now.
-  return info.endpoint;
+  return `${info.serviceId}-${info.endpoint}`;
 }
